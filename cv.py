734e03b94c0a0375fbe71351bd33d00d7ea64ea6
ambient-work <ambio.work@gmail.com>
2023-06-03 10:38:26 +0330

    openCV extractor

---
+++ cv.py
@@ -0,0 +1,28 @@
+import cv2
+import pytesseract
+import sys
+
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+img_path = sys.argv[1]
+img = cv2.imread(img_path)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+ret, thresh1 = cv2.threshold(
+    gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
+rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (18, 18))
+dilation = cv2.dilate(thresh1, rect_kernel, iterations=1)
+contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL,
+                                       cv2.CHAIN_APPROX_NONE)
+im2 = img.copy()
+file = open("recognized.txt", "w+")
+file.write("")
+file.close()
+for cnt in contours:
+    x, y, w, h = cv2.boundingRect(cnt)
+    rect = cv2.rectangle(im2, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    cropped = im2[y:y + h, x:x + w]
+    file = open("recognized.txt", "a")
+    text = pytesseract.image_to_string(cropped)
+    file.write(text)
+    file.write("\n")
+    file.close()
+print('Text extracted successfully and saved in recognized.txt')
